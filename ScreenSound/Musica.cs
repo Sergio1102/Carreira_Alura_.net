4f7ffb01a404b075104b96f2f62f3c03d38f37bc
Sergio Vinícius <74887082+Sergio1102@users.noreply.github.com>
2025-12-18 15:54:30 -0300

    Iniciando os estudos de Orientação a Objetos em C#

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,23 @@
+class Musica
+{
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => $"A música {Nome} pertence a banda {Artista}";
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine(@$"Nome: {Nome}
+Artista: {Artista}
+Duração: {Duracao}");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,8 @@
+﻿Musica musica1 = new Musica();
+musica1.Nome = "Vento no litoral";
+musica1.Artista = "Legião Urbana";
+musica1.Duracao = 366;
+musica1.Disponivel = true;
+
+Console.WriteLine(musica1.DescricaoResumida);
+musica1.ExibirFichaTecnica();
